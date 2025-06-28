4249af9615da7fe2260e10eec475e3c9fca161be
Juan Ruiz <jdruiz008@gmail.com>
2025-06-27 21:13:24 -0500

    add: i18n.js for locales, uploadConfigure to upload files with multer, loginController and swagger.yaml

--- app.js
+++ app.js
@@ -1,62 +1,92 @@
-import path from 'node:path'
-import express from 'express'
-import createError from 'http-errors'
-import logger from 'morgan'
-import connectMongoose from './lib/connectMongoose.js'
-import * as homeController from './controllers/homeController.js'
-import * as sessionManager from './lib/sessionManager.js'
-import * as loginController from './controllers/loginController.js'
-import * as productController from './controllers/productsController.js'
+import path from "node:path";
+import express from "express";
+import logger from "morgan";
+import connectMongoose from "./lib/connectMongoose.js";
+import * as homeController from "./controllers/homeController.js";
+import * as sessionManager from "./lib/sessionManager.js";
+import * as loginController from "./controllers/loginController.js";
+import * as productController from "./controllers/productsController.js";
 
-await connectMongoose()
-console.log('connected to mongoDB')
+await connectMongoose();
+console.log("connected to mongoDB");
 
+const app = express();
 
-const app = express()
+app.set("views", "views");
+app.set("view engine", "html");
+app.engine("html", (await import("ejs")).__express);
 
-app.set('views','views')
-app.set('view engine', 'html')
-app.engine('html', (await import ('ejs')).__express)
+app.locals.appName = "NodePop";
 
-app.locals.appName = 'NodePop'
+/**
+ * General rutes
+ */
 
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(import.meta.dirname, "public")));
 
-app.use(logger('dev'))
-app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(import.meta.dirname,'public')))
+/**
+ * API routes
+ */
 
+app.post("/api/login", apiLoginController.loginJWT);
+app.get("/api/agents", jwtAuth.guard, apiAgentsController.list);
+app.get("/api/agents/:agentId", jwtAuth.guard, apiAgentsController.getOne);
+app.post(
+  "/api/agents",
+  jwtAuth.guard,
+  upload.single("avatar"),
+  apiAgentsController.newAgent
+);
+app.put(
+  "/api/agents/:agentId",
+  upload.single("avatar"),
+  jwtAuth.guard,
+  apiAgentsController.upDate
+);
+app.delete(
+  "/api/agents/:agentId",
+  jwtAuth.guard,
+  apiAgentsController.deleteAgent
+);
 
 /**
  * application rutes
-*/
-app.use(sessionManager.middleware)
-app.use(sessionManager.useSessionInViews)
-app.get('/',homeController.index)
-app.get('/login',loginController.index)
-app.post('/login',loginController.loginUserPost)
-app.get('/logout',loginController.logout)
-app.get('/products/new', sessionManager.guard,productController.index )
-app.post('/products/new',sessionManager.guard,productController.addProduct)
-app.get('/products/delete/:productId', sessionManager.guard, productController.deleteProdcut)
+ */
+app.use(sessionManager.middleware);
+app.use(sessionManager.useSessionInViews);
+app.get("/", homeController.index);
+app.get("/login", loginController.index);
+app.post("/login", loginController.loginUserPost);
+app.get("/logout", loginController.logout);
+app.get("/products/new", sessionManager.guard, productController.index);
+app.post("/products/new", sessionManager.guard, productController.addProduct);
+app.get(
+  "/products/delete/:productId",
+  sessionManager.guard,
+  productController.deleteProdcut
+);
 
+app.use((err, req, res, next) => {
+  if (err.array) {
+    err.message =
+      "invalid request : " +
+      err
+        .array()
+        .map((e) => `${e.location} ${e.type} ${e.path} ${e.msg}`)
+        .join(",");
 
-app.use((err,req,res,next) => {
+    err.status = 422;
+  }
 
-    if (err.array){
-        err.message = 'invalid request : ' + err.array()
-        .map(e => `${e.location} ${e.type} ${e.path} ${e.msg}`)
-        .join(',')
+  res.status(err.status || 500);
+  // res.send('Ocurrio un error: ' + err.message)
 
-        err.status = 422
-    }
-    
-    res.status(err.status || 500)
-    // res.send('Ocurrio un error: ' + err.message) 
+  //set locals, including error informartion in development
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODEAPP_ENV === "development" ? err : {};
+  res.render("error");
+});
 
-    //set locals, including error informartion in development 
-    res.locals.message = err.message
-    res.locals.error = process.env.NODEAPP_ENV === 'development' ? err : {}
-    res.render('error')
-})
-
-export default app
+export default app;
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,42 +1,37 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
 
-
-export function index(req,res,next){
-    res.locals.error = ''
-    res.locals.email = ''
-    res.render('login')
+export function index(req, res, next) {
+  res.locals.error = "";
+  res.locals.email = "";
+  res.render("login");
 }
 
-export async function loginUserPost(req,res,next){
+export async function loginUserPost(req, res, next) {
+  try {
+    const { email, password } = req.body;
 
-    try {
-        const {email, password} = req.body
+    const redir = req.query.redir;
 
-        const redir = req.query.redir
-    
-        const foundUser = await User.findOne({email: email})
-        if(!foundUser || !(await foundUser.comparePassword(password))){
-            res.locals.error = 'Invalid user or password'
-            res.locals.email = email
-            res.render('login')
-        }
-        req.session.userId = foundUser.id
-    
-        res.redirect(redir ? redir : '/')
-        
-    } catch (error) {
-        next(error)
+    const foundUser = await User.findOne({ email: email });
+    if (!foundUser || !(await foundUser.comparePassword(password))) {
+      res.locals.error = "Invalid user or password";
+      res.locals.email = email;
+      res.render("login");
     }
+    req.session.userId = foundUser.id;
+
+    res.redirect(redir ? redir : "/");
+  } catch (error) {
+    next(error);
+  }
 }
 
-export function logout(req,res,next){
-    req.session.regenerate(err => {
-        if(err){
-            next(err)
-            return
-        }
-        res.redirect('/')
-    })
+export function logout(req, res, next) {
+  req.session.regenerate((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect("/");
+  });
 }
-
-
