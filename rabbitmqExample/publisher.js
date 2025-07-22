bc80d9c8c5c164e0450cfe419c26087e1bac0669
Juan Ruiz <jdruiz008@gmail.com>
2025-07-21 21:00:55 -0500

    publisher.js

--- rabbitmqExample/publisher.js
+++ rabbitmqExample/publisher.js
@@ -11,13 +11,30 @@ await channel.assertExchange(EXCHANGE_NAME, "direct", {
   durable: true,
 });
 
+let keepSending = true;
+
 while (true) {
   //Publicar un mensaje
   const message = {
     tarea: "enviar un email" + "-" + Date.now(),
   };
-  console.log(`Message sendt to queque at: ${Date.now()}`);
 
-  channel.publish(EXCHANGE_NAME, "*", Buffer.from(JSON.stringify(message)));
+  //esperar si puedo enviar mas mensajes o si tengo que darle mas tiempo
+  if (!keepSending) {
+    console.warn(
+      "**** CANAL SATURADO, ESPERAMOS A QUE SE DRENE EL BUFFER DE ENTRADA****"
+    );
+    //escuchar el evento drain para saber cuando ya hay espacio
+    await new Promise((resolve) => {
+      channel.on("drain", resolve);
+    });
+  }
+
+  keepSending = channel.publish(
+    EXCHANGE_NAME,
+    "*",
+    Buffer.from(JSON.stringify(message))
+  );
+  console.log(`Message sendt to queque at: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 100));
 }
