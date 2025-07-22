3c0c34ef06ede7144d1a1bd3b7fb9196e751f44f
Juan Ruiz <jdruiz008@gmail.com>
2025-07-22 11:34:44 -0500

    integrate socket in nodepop

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import "dotenv/config";
-
 import http from "node:http";
 import app from "./app.js";
+import { setupWebSocketServer } from "./webSocketServer.js";
 
 const port = process.env.PORT || 3000;
 
 //create http server
-
 const server = http.createServer(app);
+
+setupWebSocketServer(server);
 
 server.on("error", (err) => console.error(err));
 server.on("listening", () => {
